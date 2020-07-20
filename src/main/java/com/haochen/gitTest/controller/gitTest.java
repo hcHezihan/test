a16e6b26ba937a3bbdddda5541d32eb6532c456a
hcHezihan <1147522066@qq.com>
2020-07-20 16:43:02 +0800

    test

---
+++ src/main/java/com/haochen/gitTest/controller/gitTest.java
@@ -0,0 +1,12 @@
+package com.haochen.gitTest.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class gitTest {
+    /*@RequestMapping("test")
+    public Object test(){
+        return "test";
+    }*/
+}
